be33432b81204666c1ccb068cf85fb2412cac92d
FreeNikita <freenikita95@gmail.com>
2021-03-24 22:50:02 +0200

    connect redux

---
+++ src/store/reducers/products.js
@@ -0,0 +1,11 @@
+import { PRODUCTS } from "../../data/dummy-data";
+
+const initialState = {
+    availableProducts: PRODUCTS,
+    userProducts: PRODUCTS.filter(prod => prod.id === 'u1')
+}
+
+export default ( state = initialState, actions) => {
+
+    return state
+}
